986cb2b422d8933c062dd8972a46f5ee9639a6c9
joon <126473+dvessel@users.noreply.github.com>
2023-08-21 09:14:32 -0400

    zsh history tweaks.
    - still needs extended_history when trimming history.
    - lower the history limit.

--- .zshrc-source/options.zsh
+++ .zshrc-source/options.zsh
@@ -5,9 +5,10 @@
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 setopt no_case_glob
 
-setopt share_history          # Reads from history when it changes and write to 
-                              # it after every command. Output is the same as
-                              # extended_history. inc_append_history not needed.
+setopt share_history          # Reads/writes history to keep sessions in sync.
+                              # inc_append_history not needed.
+setopt extended_history       # Extended format required when the history file
+                              # is trimed. share_history shares the same format.
 setopt hist_expire_dups_first # Drop duplicate event first when trimming history.
 setopt hist_ignore_dups       # Don't save an event that was just saved again.
 setopt hist_find_no_dups      # Do not display a previously found event.
@@ -15,11 +16,11 @@ setopt hist_ignore_space      # Do not record an event starting with a space.
 setopt hist_verify            # Do not execute immediately upon history expansion.
 setopt hist_beep              # Beep when accessing non-existent history.
 
-# If the internal history needs to be trimmed to add the current command line,
-# setting hist_expire_dups_first will cause the oldest history event that has a
-# duplicate to be lost before losing a unique event from the list. You should be
-# sure to set the value of HISTSIZE to a larger number than SAVEHIST in order to
-# allow some room for the duplicated events, otherwise this option will behave
-# just like hist_ignore_all_dups once the history fills up with unique events.
-HISTSIZE=12000
-SAVEHIST=10000
+# If the history needs to be trimmed to add the current command line, setting
+# hist_expire_dups_first will cause the oldest duplicate history to be lost
+# before losing a unique entry from the list. Be sure to set the value of
+# HISTSIZE to a larger number than SAVEHIST in order to allow room for the
+# duplicated events, otherwise this option will behave like hist_ignore_all_dups
+# once the history fills up with unique events.
+HISTSIZE=6000
+SAVEHIST=5000
