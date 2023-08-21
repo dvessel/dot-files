398b7c7cf68fba244714a54a78882f152dd82939
joon <126473+dvessel@users.noreply.github.com>
2023-08-20 20:18:58 -0400

    Clean up zsh options.

--- .zshrc-source/options.zsh
+++ .zshrc-source/options.zsh
@@ -1,21 +1,25 @@
+
+# https://zsh.sourceforge.io/Doc/Release/Options.html
+
 # Case insensitive autocomplete and globbing.
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 setopt no_case_glob
 
-setopt bang_hist              # Treat the '!' character specially during expansion.
-setopt extended_history       # Write the history file in the ':start:elapsed;command' format.
-setopt inc_append_history     # Write to the history file immediately, not when the shell exits.
-setopt hist_expire_dups_first # Expire a duplicate event first when trimming history.
-setopt hist_ignore_dups       # Do not record an event that was just recorded again.
-setopt hist_ignore_all_dups   # Delete an old recorded event if a new event is a duplicate.
+setopt share_history          # Reads from history when it changes and write to 
+                              # it after every command. Output is the same as
+                              # extended_history. inc_append_history not needed.
+setopt hist_expire_dups_first # Drop duplicate event first when trimming history.
+setopt hist_ignore_dups       # Don't save an event that was just saved again.
 setopt hist_find_no_dups      # Do not display a previously found event.
 setopt hist_ignore_space      # Do not record an event starting with a space.
-setopt hist_save_no_dups      # Do not write a duplicate event to the history file.
 setopt hist_verify            # Do not execute immediately upon history expansion.
 setopt hist_beep              # Beep when accessing non-existent history.
 
-HISTSIZE=10000
+# If the internal history needs to be trimmed to add the current command line,
+# setting hist_expire_dups_first will cause the oldest history event that has a
+# duplicate to be lost before losing a unique event from the list. You should be
+# sure to set the value of HISTSIZE to a larger number than SAVEHIST in order to
+# allow some room for the duplicated events, otherwise this option will behave
+# just like hist_ignore_all_dups once the history fills up with unique events.
+HISTSIZE=12000
 SAVEHIST=10000
-
-editor='code -w'
-git_editor='code -w'
