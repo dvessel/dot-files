defca6de7237558b5fc8c8e5a901f81065b7b61d
joon <126473+dvessel@users.noreply.github.com>
2023-11-13 12:24:16 -0500

    remove outdated screen capture commands. Add launchctl restart. OBS won't let go.

--- .zshrc-source/aliases.zsh
+++ .zshrc-source/aliases.zsh
@@ -31,19 +31,4 @@ alias sync.all='sync.emulation && sync.storage'
 alias hex2decimal='printf "%d\n"'
 alias decimal2hex='printf "0x0%x\n"'
 
-alias capture.fullscreen='screencapture -vpdR 0,0,1728,1117 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.no-menu='screencapture -vpdR 0,37,1728,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.4/3-center='screencapture -vpdR 144,37,1440,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.left='screencapture -vpdR 0,37,864,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.right='screencapture -vpdR 864,37,864,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.3/4-left='screencapture -vpdR 27,37,810,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.3/4-center='screencapture -vpdR 459,37,810,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
-alias capture.3/4-right='screencapture -vpdR 891,37,810,1080 \
-  -G com.rogueamoeba.Loopback:4699A16B-2B90-4A3F-B522-B2CFFAC463FA'
+alias capture.restart='launchctl kickstart -pk gui/$UID/com.apple.replayd'
