27524b07d2bae257f679d41256a99ab7bb281090
joon <126473+dvessel@users.noreply.github.com>
2024-02-26 06:37:53 -0500

    Update sync aliases.

--- .zshrc-source/aliases.zsh
+++ .zshrc-source/aliases.zsh
@@ -20,9 +20,11 @@ alias unquarantine='xattr -d com.apple.quarantine'
 
 alias sync.emulation='rsync -avL --itemize-changes --delete \
   ~/Games/Emulation /Volumes/980Pro'
-alias sync.storage='rsync -avzL --delete --progress -h \
+alias sync.storage.local='rsync -avL --itemize-changes --delete \
+  ~/Games/Support/OpenEmu/AdvanceScan/_{unknown,import/_updates} /Volumes/Storage/Emulation/MAME'
+alias sync.storage.remote='rsync -avzL --delete --progress -h \
   /Volumes/Storage/* dvessel-ds.local:/volume1/storage'
-alias sync.all='sync.emulation && sync.storage'
+alias sync.all='sync.emulation && sync.storage.local && sync.storage.remote'
 
 alias hex2decimal='printf "%d\n"'
 alias decimal2hex='printf "0x0%x\n"'
