85e8018cb084a1fdc6404ee48b3f5e0440f6f74e
joon <126473+dvessel@users.noreply.github.com>
2024-04-08 14:02:09 -0400

    cleanup aliases.

--- .zshrc-source/aliases.zsh
+++ .zshrc-source/aliases.zsh
@@ -21,13 +21,12 @@ alias show-ext='SetFile -a e'
 
 alias unquarantine='xattr -d com.apple.quarantine'
 
-alias sync-emulation='rsync -avL --itemize-changes --delete \
-  ~/Games/Emulation /Volumes/980Pro'
-alias sync-storage-local='rsync -avL --itemize-changes --delete \
+alias hex2decimal='printf "%d\n"'
+alias decimal2hex='printf "0x0%x\n"'
+
+alias sync-emulation='rsync -avL --itemize-changes --delete ~/Games/Emulation /Volumes/980Pro'
+alias sync-mame-storage='rsync -avL --itemize-changes --delete \
   ~/Games/Support/OpenEmu/AdvanceScan/_{unknown,import/_updates} /Volumes/Storage/Emulation/MAME'
 alias sync-storage-remote='rsync -avzL --delete --progress -h \
   /Volumes/Storage/* dvessel-ds.local:/volume1/storage'
-alias sync-all='sync-emulation && sync-storage-local && sync-storage-remote'
-
-alias hex2decimal='printf "%d\n"'
-alias decimal2hex='printf "0x0%x\n"'
+alias sync-all='sync-emulation && sync-mame-storage && sync-storage-remote'
