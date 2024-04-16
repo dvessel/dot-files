d76088e56ddc765e6c315223c0c09381a9fb0bbd
joon <126473+dvessel@users.noreply.github.com>
2024-04-16 06:45:32 -0400

    clean fzf options.

--- .zshrc-source/fzf.zsh
+++ .zshrc-source/fzf.zsh
@@ -1,13 +1,14 @@
 _brew_check fzf || return
 
-FZF_DEFAULT_OPTS='--reverse --scroll-off 6 --info inline --pointer ▏'
-FZF_DEFAULT_OPTS+=' --marker "✸ " --ellipsis … --preview-window noborder'
-FZF_DEFAULT_OPTS+=' --color gutter:-1,hl+:underline,hl:underline'
-export FZF_DEFAULT_OPTS
+_opts=(
+  --reverse
+  --scroll-off 7
+  --info inline
+  --pointer ▏
+  --marker '✸ '
+  --ellipsis …
+  --preview-window noborder
+  --color gutter:-1,hl+:underline,hl:underline
+)
+export FZF_DEFAULT_OPTS=${_opts[@]}; unset _opts
 export FZF_COMPLETION_TRIGGER='..'
-
-if _brew_check fd; then
-  export FZF_DEFAULT_COMMAND='fd --type f'
-  _fzf_compgen_path() { fd --follow . "$1" }
-  _fzf_compgen_dir() { fd --type d --follow . "$1" }
-fi
