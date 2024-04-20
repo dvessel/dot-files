cdf2e57fc032c01dab70a90feb98bcafe558f5d5
joon <126473+dvessel@users.noreply.github.com>
2024-04-20 11:21:40 -0400

    Update sync-* aliases to ignore .DS_Store files.

--- .zshrc-source/odds-n-ends.zsh
+++ .zshrc-source/odds-n-ends.zsh
@@ -5,10 +5,10 @@ compdef fbrew=brew
 
 alias lcut='/Applications/LosslessCut.app/Contents/MacOS/LosslessCut'
 
-alias sync-emulation='rsync -avL --itemize-changes --delete ~/Games/Emulation /Volumes/980Pro'
-alias sync-mame-storage='rsync -avL --itemize-changes --delete \
+alias sync-emulation='rsync -avL --itemize-changes --delete --exclude=.DS_Store \
+  ~/Games/Emulation /Volumes/980Pro'
+alias sync-mame-storage='rsync -avL --itemize-changes --delete --exclude=.DS_Store \
   ~/Games/Support/OpenEmu/AdvanceScan/_{unknown,import/_updates} /Volumes/Storage/Emulation/MAME'
-alias sync-storage-remote='rsync -avzL --delete --progress -h \
+alias sync-storage-remote='rsync -avzL --delete --progress -h --exclude=.DS_Store\
   /Volumes/Storage/* dvessel-ds.local:/volume1/storage'
 alias sync-all='sync-emulation && sync-mame-storage && sync-storage-remote'
-
