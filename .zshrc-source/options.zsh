22d7cebabaa9ea1c66760307f29cc174f0278712
joon <126473+dvessel@users.noreply.github.com>
2024-04-27 10:25:06 -0400

    nvim to EDITOR/VISUAL.

--- .zshrc-source/options.zsh
+++ .zshrc-source/options.zsh
@@ -31,3 +31,6 @@ SAVEHIST=5000
 setopt AUTO_PUSHD         # Push the current directory visited on the stack.
 setopt PUSHD_IGNORE_DUPS  # Do not store duplicates in the stack.
 setopt PUSHD_SILENT       # Do not print the directory stack after pushd or popd.
+
+VISUAL=nvim
+EDITOR=nvim
