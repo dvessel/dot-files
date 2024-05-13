0d58aa90c401328d2d05e989f730d99715f5aa3b
joon <126473+dvessel@users.noreply.github.com>
2024-05-13 10:49:31 -0400

    Export VISUAL/EDITOR.
    looks like it's necessary.

--- .zshrc-source/options.zsh
+++ .zshrc-source/options.zsh
@@ -33,6 +33,6 @@ setopt AUTO_PUSHD         # Push the current directory visited on the stack.
 setopt PUSHD_IGNORE_DUPS  # Do not store duplicates in the stack.
 setopt PUSHD_SILENT       # Do not print the directory stack after pushd or popd.
 
-VISUAL=nvim
-EDITOR=nvim
+export VISUAL=nvim
+export EDITOR=nvim
 export LESS='-R --mouse'
