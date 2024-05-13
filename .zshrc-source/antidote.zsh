2fbd64f72e1e44f1b7cb12fb982222907e6f847f
joon <126473+dvessel@users.noreply.github.com>
2024-05-13 16:45:29 -0400

    history-substring plugin should be enabled before setting keybinds.

--- .zshrc-source/antidote.zsh
+++ .zshrc-source/antidote.zsh
@@ -11,8 +11,10 @@ GENCOMPL_FPATH=~/.local/zcompletions
 # mattmc3/zephyr plugins:zman
 ZMAN_URL=https://zsh-manual.netlify.app
 
-# zsh-users/zsh-history-substring-search
-for k in '^[[A' '^P'; bindkey $k history-substring-search-up
-for k in '^[[B' '^N'; bindkey $k history-substring-search-down
-HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
-HISTORY_SUBSTRING_SEARCH_FUZZY=1
+if type history-substring-search-up &>/dev/null; then
+  # zsh-users/zsh-history-substring-search
+  for k in '^[[A' '^P'; bindkey $k history-substring-search-up
+  for k in '^[[B' '^N'; bindkey $k history-substring-search-down
+  HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+  HISTORY_SUBSTRING_SEARCH_FUZZY=1
+fi
