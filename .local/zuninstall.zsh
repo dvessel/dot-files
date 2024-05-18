2aa0e5aa4bdf55af4007e00464b821db774b6961
joon <126473+dvessel@users.noreply.github.com>
2024-05-18 13:48:10 -0400

    Suppress missing path warnings.

--- .local/zuninstall.zsh
+++ .local/zuninstall.zsh
@@ -4,7 +4,7 @@ print "Uninstalling homebrew.sh…"
 
 if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 then
-  __caches=( ~/.cache/{antidote,gitstatus,p10k-*,zsh} )
+  __caches=( ~/.cache/{antidote,gitstatus,p10k-*,zsh}(N) )
   print "Removing caches…"
   printf "%s\n" $__caches
   rm -fr $__caches
