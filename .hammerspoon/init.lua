6393784e7a6441c406ab7fd567d6d535debe8619
joon <126473+dvessel@users.noreply.github.com>
2024-06-03 17:52:05 -0400

    hammerspoon, simplify ctrl top to escape.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,40 @@
 hs.ipc.cliInstall()
 
-require('control-escape')
-
-keyUpDown = function(modifiers, key)
+local keyUpDown = function(modifiers, key)
   hs.eventtap.keyStroke(modifiers, key, 0)
 end
 
+-- tap ctrl to send escape
+
+local send_esc = false
+local last_mod = {}
+local ctrl_key_timer = hs.timer.delayed.new(0.20, function()
+  send_esc = false
+end)
+
+hs.eventtap.new({ hs.eventtap.event.types.flagsChanged }, function(evt)
+  local new_mod = evt:getFlags()
+
+  if last_mod["ctrl"] == new_mod["ctrl"] then
+    return false
+  end
+
+  if not last_mod["ctrl"] then
+    last_mod = new_mod
+    send_esc = true
+    ctrl_key_timer:start()
+  else
+    if send_esc then
+      keyUpDown({}, "escape")
+    end
+    last_mod = new_mod
+    ctrl_key_timer:stop()
+  end
+
+  return false
+end):start()
+
+hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function()
+  send_esc = false
+  return false
+end):start()
