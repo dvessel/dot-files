a1d9e970c0f7a58896e6021589129e6a3dead4f2
joon <126473+dvessel@users.noreply.github.com>
2024-06-09 19:13:59 -0400

    smarter window cycle through hammerspoon.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -41,3 +41,17 @@ OtherTap = hs.eventtap.new({ hs.eventtap.event.types.keyDown }, function()
   sendEsc = false
   return false
 end):start()
+
+-- window switcher
+Switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true), {
+  fontName = 'SF Compact Display',
+  backgroundColor = {0, 0, 0, 0.5},
+  highlightColor = {0.2, 0.2, 0.2, 0.5},
+  showSelectedThumbnail = false,
+  showThumbnails = false,
+  textColor = {1, 1, 1},
+  textSize = 12,
+  titleBackgroundColor = {alpha=0}
+})
+hs.hotkey.bind('cmd', '`', nil, function() Switcher:next() end)
+hs.hotkey.bind('cmd-shift', '`', nil, function() Switcher:previous() end)
