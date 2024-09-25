93a67e9a5a5f677ca6d127288621e46ce88a479a
joon <126473+dvessel@users.noreply.github.com>
2024-09-25 13:58:26 -0400

    fzf-tab now ignores FZF_DEFAULT_OPTS. 0b49f3e8a3fdb546636ff2157925d32e8936b415

--- .zshrc-source/fzf.zsh
+++ .zshrc-source/fzf.zsh
@@ -5,10 +5,12 @@ _opts=(
   --scroll-off 7
   --info inline
   --pointer ▏
-  --marker '✸\ '
+  --marker +
   --ellipsis …
   --preview-window noborder
   --color gutter:-1,hl+:underline,hl:underline
 )
+zstyle ':fzf-tab:complete:*' fzf-flags $_opts
+
 export FZF_DEFAULT_OPTS=${_opts[@]}; unset _opts
 export FZF_COMPLETION_TRIGGER='..'
