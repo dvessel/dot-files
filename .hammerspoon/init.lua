bb5a02218bee9771b36fad2c92423f303e24628b
joon <126473+dvessel@users.noreply.github.com>
2024-11-14 14:45:07 -0500

    Renable previous hammerspoon config.
    And disable conflicting OS keybindings.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,27 +1,27 @@
--- require('hs.ipc')
+require('hs.ipc')
 
--- hs.loadSpoon("ReloadConfiguration")
+hs.loadSpoon("ReloadConfiguration")
 hs.loadSpoon('ControlEscape')
 
--- spoon.ReloadConfiguration:start()
+spoon.ReloadConfiguration:start()
 spoon.ControlEscape:start()
 
 -- window switcher
--- local uiPrefs = {
---   fontName = 'SF Compact Display',
---   backgroundColor = {0, 0, 0, 0.7},
---   highlightColor = {0.5, 0.5, 0.5, 0.5},
---   showSelectedThumbnail = false,
---   textColor = {1, 1, 1},
---   textSize = 14,
---   thumbnailSize = 128,
---   titleBackgroundColor = {alpha=0},
--- }
+local uiPrefs = {
+  fontName = 'SF Compact Display',
+  backgroundColor = {0, 0, 0, 0.7},
+  highlightColor = {0.5, 0.5, 0.5, 0.5},
+  showSelectedThumbnail = false,
+  textColor = {1, 1, 1},
+  textSize = 14,
+  thumbnailSize = 128,
+  titleBackgroundColor = {alpha=0},
+}
 
--- SwitcherSpaceAll = hs.window.switcher.new(hs.window.filter.new(), uiPrefs)
--- hs.hotkey.bind('cmd', '`', nil, function() SwitcherSpaceAll:next() end)
--- hs.hotkey.bind('cmd-shift', '`', nil, function() SwitcherSpaceAll:previous() end)
+SwitcherSpaceAll = hs.window.switcher.new(hs.window.filter.new(), uiPrefs)
+hs.hotkey.bind('cmd', '`', nil, function() SwitcherSpaceAll:next() end)
+hs.hotkey.bind('cmd-shift', '`', nil, function() SwitcherSpaceAll:previous() end)
 
--- SwitcherSpace = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true), uiPrefs)
--- hs.hotkey.bind('alt', '`', nil, function() SwitcherSpace:next() end)
--- hs.hotkey.bind('alt-shift', '`', nil, function() SwitcherSpace:previous() end)
+SwitcherSpace = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true), uiPrefs)
+hs.hotkey.bind('alt', '`', nil, function() SwitcherSpace:next() end)
+hs.hotkey.bind('alt-shift', '`', nil, function() SwitcherSpace:previous() end)
