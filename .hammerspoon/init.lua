484ef089c1c939556283cc55f79371cbeab2462f
joon <126473+dvessel@users.noreply.github.com>
2024-11-21 19:39:13 -0500

    Less buggy hammerspoon window switcher.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,7 +2,14 @@ require('hs.ipc')
 
 hs.loadSpoon("ReloadConfiguration")
 hs.loadSpoon('ControlEscape')
+hs.loadSpoon("AppWindowSwitcher")
 -- hs.loadSpoon('EmmyLua')
 
 spoon.ReloadConfiguration:start()
 spoon.ControlEscape:start()
+spoon.AppWindowSwitcher:bindHotkeys({
+   ["com.apple.Safari"]       = {"shift-ctrl", "1"},
+   ["com.vscodium"]           = {"shift-ctrl", "2"},
+   ["com.googlecode.iterm2"]  = {"shift-ctrl", "3"},
+   ["com.apple.finder"]       = {"shift-ctrl", "4"},
+})
