ffa71ee9f4e5f6a84eccdad435a771186a2147e1
joon <126473+dvessel@users.noreply.github.com>
2024-11-21 19:40:31 -0500

    Making quitting more deliberate.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -2,11 +2,13 @@ require('hs.ipc')
 
 hs.loadSpoon("ReloadConfiguration")
 hs.loadSpoon('ControlEscape')
+hs.loadSpoon("HoldToQuit")
 hs.loadSpoon("AppWindowSwitcher")
 -- hs.loadSpoon('EmmyLua')
 
 spoon.ReloadConfiguration:start()
 spoon.ControlEscape:start()
+spoon.HoldToQuit:start()
 spoon.AppWindowSwitcher:bindHotkeys({
    ["com.apple.Safari"]       = {"shift-ctrl", "1"},
    ["com.vscodium"]           = {"shift-ctrl", "2"},
