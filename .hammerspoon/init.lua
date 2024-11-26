efa955e9835d613a9e6f59d54111ac926afd2c4e
joon <126473+dvessel@users.noreply.github.com>
2024-11-26 14:38:35 -0500

    Alter keybinds for hammerspoon window switching.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -10,9 +10,9 @@ spoon.ReloadConfiguration:start()
 spoon.ControlEscape:start()
 spoon.HoldToQuit:start()
 spoon.AppWindowSwitcher:bindHotkeys({
-   [""]                       = {"shift-ctrl", "1"},
-   ["com.apple.Safari"]       = {"shift-ctrl", "2"},
-   ["com.vscodium"]           = {"shift-ctrl", "3"},
-   ["com.googlecode.iterm2"]  = {"shift-ctrl", "4"},
-   ["com.apple.finder"]       = {"shift-ctrl", "5"},
+   [""]                       = {"shift-ctrl", "`"},
+   ["com.apple.Safari"]       = {"shift-ctrl", "1"},
+   ["com.vscodium"]           = {"shift-ctrl", "2"},
+   ["com.googlecode.iterm2"]  = {"shift-ctrl", "3"},
+   ["com.apple.finder"]       = {"shift-ctrl", "4"},
 })
