27cc9fa2db9d09e21ba26157273c69e7311d357a
joon <126473+dvessel@users.noreply.github.com>
2025-01-15 23:44:50 -0500

    more displayplacer aliases.

--- .zshrc-source/odds-n-ends.zsh
+++ .zshrc-source/odds-n-ends.zsh
@@ -14,12 +14,33 @@ if ! type nvim &>/dev/null && test -f /opt/homebrew/bin/nvim; then
 fi
 alias lcut='/Applications/LosslessCut.app/Contents/MacOS/LosslessCut'
 
-alias dpnative2x1.6='displayplacer "id:1 mode:48"'
-alias dpnative2x='displayplacer "id:1 mode:54"'
-alias dpmore2x1.6='displayplacer "id:1 mode:66"'
-alias dpmore2x-default='displayplacer "id:1 mode:72"'
-alias dpnative1x='displayplacer "id:1 mode:126"'
+if _brew_check -s displayplacer; then
 
+  alias dpmore:120hz='displayplacer     "id:1 mode:72"'
+  alias dpmore:60hz='displayplacer      "id:1 mode:73"'
+  alias dpmore:59.94hz='displayplacer   "id:1 mode:74"'
+  alias dpmore:50hz='displayplacer      "id:1 mode:75"'
+  alias dpmore:48hz='displayplacer      "id:1 mode:76"'
+
+  alias dpmore-notchless:120hz='displayplacer     "id:1 mode:66"'
+  alias dpmore-notchless:60hz='displayplacer      "id:1 mode:67"'
+  alias dpmore-notchless:59.94hz='displayplacer   "id:1 mode:68"'
+  alias dpmore-notchless:50hz='displayplacer      "id:1 mode:69"'
+  alias dpmore-notchless:48hz='displayplacer      "id:1 mode:70"'
+
+  alias dpnative:120hz='displayplacer   "id:1 mode:54"'
+  alias dpnative:60hz='displayplacer    "id:1 mode:55"'
+  alias dpnative:59.94hz='displayplacer "id:1 mode:56"'
+  alias dpnative:50hz='displayplacer    "id:1 mode:57"'
+  alias dpnative:48hz='displayplacer    "id:1 mode:58"'
+
+  alias dpnative-notchless:120hz='displayplacer   "id:1 mode:48"'
+  alias dpnative-notchless:60hz='displayplacer    "id:1 mode:49"'
+  alias dpnative-notchless:59.94hz='displayplacer "id:1 mode:50"'
+  alias dpnative-notchless:50hz='displayplacer    "id:1 mode:51"'
+  alias dpnative-notchless:48hz='displayplacer    "id:1 mode:52"'
+
+fi
 
 alias sync-980-emulation='rsync -avL --itemize-changes --delete --exclude=.DS_Store \
   ~/Games/Emulation /Volumes/980Pro'
