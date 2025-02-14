e732651dedc9e04df0bbbfc6b2d3e4cf48c1bf7e
joon <126473+dvessel@users.noreply.github.com>
2025-02-14 16:10:50 -0500

    .zsource/fzf.zsh minor cleanup.

--- .zsource/fzf.zsh
+++ .zsource/fzf.zsh
@@ -3,7 +3,7 @@
 # https://github.com/junegunn/fzf#setting-up-shell-integration
 source <(fzf --zsh)
 
-_opts=(
+opts=(
   --reverse
   --scroll-off 7
   --info inline
@@ -13,7 +13,9 @@ _opts=(
   --preview-window noborder
   --color gutter:-1,hl+:underline,hl:underline
 )
-zstyle ':fzf-tab:complete:*' fzf-flags $_opts
+zstyle ':fzf-tab:complete:*' fzf-flags $opts
 
-export FZF_DEFAULT_OPTS=${_opts[@]}; unset _opts
+export FZF_DEFAULT_OPTS=${opts[@]}
 export FZF_COMPLETION_TRIGGER='..'
+
+unset opts
