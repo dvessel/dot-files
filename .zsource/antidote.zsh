e92c76aebad8ac3481f33e326df0c2a1a581ea22
joon <126473+dvessel@users.noreply.github.com>
2025-02-17 04:39:06 -0500

    slight antidote optimizations.

--- .zsource/antidote.zsh
+++ .zsource/antidote.zsh
@@ -1,25 +1,39 @@
 #!/usr/bin/env zsh
 
-if source `brew --prefix antidote`/share/antidote/antidote.zsh &>/dev/null; then
-  # Plug-in options. @see .zsh_plugins
+# Plug-in options. @see ~/.zplugins
 
-  # - mattmc3/ez-compinit
-  zstyle ':plugin:ez-compinit' 'compstyle' 'zshzoo'
-  zstyle ':plugin:ez-compinit' 'use-cache' 'yes'
+# - mattmc3/ez-compinit
+zstyle ':plugin:ez-compinit' 'compstyle' 'zshzoo'
+zstyle ':plugin:ez-compinit' 'use-cache' 'yes'
 
-  # - mattmc3/zephyr plugins:zfunctions
-  export ZFUNCDIR=~/.local/zfunctions
+# - mattmc3/zephyr plugins:zfunctions
+export ZFUNCDIR=~/.local/zfunctions
 
-  # - dvessel/zsh-completion-generator
-  export GENCOMPL_FPATH=~/.local/zfunctions
+# - dvessel/zsh-completion-generator
+export GENCOMPL_FPATH=~/.local/zfunctions
+
+# Lazy-load antidote from its functions directory.
+fpath=( `brew --prefix antidote`/share/antidote/functions $fpath)
+autoload -Uz antidote
+
+if type antidote &>/dev/null; then
+
+  zplugins=~/.zplugins
+  zpstatic=~/.zsource/zplugins!zsh
 
   export ANTIDOTE_HOME=~/.cache/antidote
   zstyle ':antidote:bundle' use-friendly-names 'yes'
-  zstyle ':antidote:bundle' file ~/.zplugins
-  zstyle ':antidote:static' file ~/.zsource/zplugins!zsh
+  zstyle ':antidote:bundle' file $zplugins
+  zstyle ':antidote:static' file $zpstatic
   zstyle ':antidote:*' zcompile 'yes'
 
-  antidote load
+  if [[ ! $zpstatic -nt $zplugins ]]; then
+    # Generate new static file when zplugins is updated.
+    antidote load
+  else
+    # Manually source when antidote doesn't need to load.
+    source $zpstatic
+  fi
 else
   # Normally handled by antidote plugin:
   # - mattmc3/ez-compinit
