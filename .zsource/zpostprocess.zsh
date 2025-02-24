9f189a645c14d2cd9056016a18a174e515d754b2
joon <126473+dvessel@users.noreply.github.com>
2025-02-24 18:10:55 -0500

    Move compinit to zpostprocess.
    Also points zcompdump to the cache directory.

--- .zsource/zpostprocess.zsh
+++ .zsource/zpostprocess.zsh
@@ -2,3 +2,9 @@
 
 # Tell `p10k configure` which file it should overwrite.
 typeset -g POWERLEVEL9K_CONFIG_FILE=$HOME/.zsource/p10k.zsh
+
+if ! type compinit >/dev/null; then
+  # Normally handled by antidote plugin:
+  # @see ~/.zplugins - mattmc3/ez-compinit
+  autoload -Uz compinit; compinit -d $XDG_CACHE_HOME/zsh/zcompdump
+fi
