6542c3716277c3163b14e9659e5ac7c88d63a21e
joon <126473+dvessel@users.noreply.github.com>
2025-02-24 06:30:10 -0500

    update options, history size, etc.

--- .zsource/options.zsh
+++ .zsource/options.zsh
@@ -9,33 +9,25 @@ zstyle ':completion:*' cache-path ${XDG_CACHE_HOME:-~/.cache}/zsh/zcompcache
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 setopt no_case_glob
 
-setopt share_history          # Reads/writes history to keep sessions in sync.
-                              # inc_append_history not needed.
-setopt extended_history       # Extended format required when the history file
-                              # is trimed. share_history shares the same format.
-setopt hist_expire_dups_first # Drop duplicate event first when trimming history.
-setopt hist_ignore_all_dups   # Delete an old recorded event if a new event is a duplicate.
-setopt hist_ignore_dups       # Don't save an event that was just saved again.
-setopt hist_find_no_dups      # Do not display a previously found event.
-setopt hist_ignore_space      # Do not record an event starting with a space.
-setopt hist_reduce_blanks     # Remove extra blanks from commands added to the history list.
-setopt hist_save_no_dups      # Do not write a duplicate event to the history file.
-setopt hist_verify            # Do not execute immediately upon history expansion.
-setopt hist_beep              # Beep when accessing non-existent history.
-
-# If the history needs to be trimmed to add the current command line, setting
-# hist_expire_dups_first will cause the oldest duplicate history to be lost
-# before losing a unique entry from the list. Be sure to set the value of
-# HISTSIZE to a larger number than SAVEHIST in order to allow room for the
-# duplicated events, otherwise this option will behave like hist_ignore_all_dups
-# once the history fills up with unique events.
-export HISTSIZE=10000
-export SAVEHIST=100000
-export HISTFILE=${XDG_DATA_HOME:-~/.local/share}/zsh/zhistory
-
 setopt auto_pushd         # Push the current directory visited on the stack.
 setopt pushd_ignore_dups  # Do not store duplicates in the stack.
 setopt pushd_silent       # Do not print the directory stack after pushd or popd.
+
+setopt share_history          # Reads/writes history to keep sessions in sync.
+                              # Overlaps with inc_append_history & extended_history.
+setopt hist_expire_dups_first # Drop duplicate event first when trimming history.
+setopt hist_save_no_dups      # Do not write a duplicate event to the history file.
+setopt hist_ignore_dups       # Don't save an event that was just saved again.
+setopt hist_ignore_space      # Remove record an event starting with a space.
+setopt hist_reduce_blanks     # Remove superfluous blanks from each command line.
+setopt hist_find_no_dups      # Do not display a previously found event.
+setopt hist_verify            # Do not execute immediately upon history expansion.
+
+export HISTFILE=${XDG_DATA_HOME:-~/.local/share}/zsh/zhistory
+export HISTSIZE=1000000
+export SAVEHIST=833333    # 833333 x 1.2 = 999,999.6 | Stay under HISTSIZE.
+                          # With `share_history` enabled, the history file will be
+                          # trimmed and rewritten when it grows 20% of SAVEHIST.
 
 # Enable run-help
 # https://zsh.sourceforge.io/Doc/Release/User-Contributions.html#Accessing-On_002dLine-Help
@@ -43,11 +35,10 @@ export HELPDIR=/usr/share/zsh/`zsh --version | cut -d ' ' -f2`/help
 unalias run-help 2>/dev/null
 autoload run-help
 
-# PATH is cleared on arch switch. Make it available in x86. @see arch-toggle
-# fallback to OS provided vim.
 if type nvim &>/dev/null; then
   export EDITOR=nvim
 elif test -r /opt/homebrew/bin/nvim; then
+  # PATH is cleared on arch switch. Make it available in x86. @see arch-toggle
   export EDITOR=/opt/homebrew/bin/nvim
 else
   export EDITOR=vim
