f3f38f7c509cc76241a57b143414aa9779a1b15f
joon <126473+dvessel@users.noreply.github.com>
2025-02-24 18:11:13 -0500

    fzf style update.

--- .zsource/fzf.zsh
+++ .zsource/fzf.zsh
@@ -4,12 +4,14 @@ local opts=(
   --reverse
   --scroll-off 7
   --info inline
-  --pointer ▏
-  --marker +
+  --pointer ┃
+  --marker ⁕ 
   --ellipsis …
-  --preview-window noborder
-  --color gutter:-1,hl+:underline,hl:underline
+  --preview-window border-rounded
+  --color pointer:red,marker:red,hl+:-1
+  --color gutter:-1,fg+:-1,bg+:-1,hl+:underline,hl:underline
 )
+# @see ~/.zplugins - Aloxaf/fzf-tab
 zstyle ':fzf-tab:complete:*' fzf-flags $opts
 
 export FZF_DEFAULT_OPTS=${opts[@]}
