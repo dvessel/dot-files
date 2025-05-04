506205e907a25ae3fc66945d7c7dff89152e793e
joon <126473+dvessel@users.noreply.github.com>
2025-05-04 15:02:52 -0400

    clean up hammerspoon.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,1 @@
--- require('hs.ipc')
-
--- hs.loadSpoon('EmmyLua')
-hs.loadSpoon("ReloadConfiguration")
-hs.loadSpoon('ControlEscape')
-hs.loadSpoon("AppWindowSwitcher")
-
-spoon.ReloadConfiguration:start()
-spoon.ControlEscape:start()
-spoon.AppWindowSwitcher:bindHotkeys({
-  ["Safari"]   = {"shift-ctrl", "1"},
-  ["VSCodium"] = {"shift-ctrl", "2"},
-  ["Ghostty"]  = {"shift-ctrl", "3"},
-  ["Finder"]   = {"shift-ctrl", "4"},
-})
+hs.loadSpoon('ControlEscape'):start()
