9eaba70af9fff30a04310ea364d1fa2a1fd534dd
joon <126473+dvessel@users.noreply.github.com>
2025-10-28 13:16:31 -0400

    re-enable antidote compinit cache.
    remember to zcc after updates or changes in command line environment.

--- .config/zsh/.zsource/antidote.zsh
+++ .config/zsh/.zsource/antidote.zsh
@@ -9,7 +9,7 @@ if test -d $HOMEBREW_PREFIX/opt/antidote; then
 
   # - mattmc3/ez-compinit
   zstyle ':plugin:ez-compinit' 'compstyle' 'zshzoo'
-  zstyle ':plugin:ez-compinit' 'use-cache' 'no'
+  zstyle ':plugin:ez-compinit' 'use-cache' 'yes'
 
   # - dvessel/zsh-completion-generator
   export GENCOMPL_FPATH=$ZFUNCDIR
